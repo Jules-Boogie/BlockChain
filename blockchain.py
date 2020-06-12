2497bfd9ce74f8923f211f3055af2d71b5b755a4
Juliet George <georgejuliet11@gmail.com>
2020-06-12 15:27:40 -0700

    add python styling

--- blockchain.py
+++ blockchain.py
@@ -1,7 +1,32 @@
 
-blockChainList = []
+blockChainList = [1]
 
-def addtoBlockList(x):
-    blockChainList.append(x)
-    print(blockChainList)
-addtoBlockList(5)
+def get_last_amount():
+    """
+    Retrieves the last value entered into the BlockChainList
+    
+
+    Arguments:
+        :None
+    """
+    return blockChainList[-1]
+
+
+def add_to_blockList(current_amount, last_amount):
+    """
+    Adds a last value in the blockchain and a new value to the blockchainList
+
+
+    Arguments:
+        :current_amount: the last value in the blockChainList
+        :last_amount: the new value to be added to the blockChainlist
+    """
+    blockChainList.append([current_amount,last_amount])
+    
+add_to_blockList(get_last_amount(), 3)
+
+# get user input
+
+user_input = float(input("please enter transaction amount: "))
+add_to_blockList(get_last_amount(), user_input)
+print(blockChainList)
